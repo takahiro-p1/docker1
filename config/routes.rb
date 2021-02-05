53a011a4d4dc340ecc57dc693fb896877de2992d
Taniguchi <takahirotakumi1@gmail.com>
2021-02-05 11:21:58 +0900

    finish09

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   get '/signup',    to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  delete   '/logout',   to: 'sessions#destroy'
+  
   resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
