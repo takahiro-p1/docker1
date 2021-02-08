0aa15a7adf7ef22a0ca39b31d13129818a5846e7
Taniguchi <takahirotakumi1@gmail.com>
2021-02-08 11:24:48 +0900

    finish11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,22 @@ class ApplicationController < ActionController::Base
         render html: 'hello,world!'
     end
 
+    private
+     # ログイン済みユーザーかどうか確認
+   def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+   end
+
+      # 現在ログインしているユーザーを返す (ユーザーがログイン中の場合のみ)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,9 @@ User.create!(name:  name,
       activated: true,
       activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = "aaaaa"
+  users.each { |user| user.microposts.create!(content: content) }
+end
